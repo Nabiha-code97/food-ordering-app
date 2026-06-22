190741bd07883d1f62c8dcf2296538d1895b2f60
Nabiha-code97 <nabihaamir97@gmail.com>
2026-06-22 14:50:02 +0500

    setup DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
-import express from "express"
-import type {Request, Response} from "express"
-import cors from "cors"
-import "dotenv/config"
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+
+import connectDB from "./config/db.ts";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async(req: Request, res: Response)=>{
-    res.json({message: "hello!"})
-})
+connectDB();
 
-app.listen(4000, ()=>{
-    console.log("Server running at port 4000")
-})
+app.get("/test", (req: Request, res: Response) => {
+    res.json({ message: "hello!" });
+});
+
+app.listen(4000, () => {
+    console.log("Server running at port 4000");
+});
