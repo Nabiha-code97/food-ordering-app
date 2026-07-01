bbae7341e7271829d2bfa0e2485562da4c507260
Nabiha-code97 <nabihaamir97@gmail.com>
2026-07-01 11:19:06 +0500

    feat:Loader added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
     },
   },
 });
-
+document.getElementById("loader")?.remove();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
